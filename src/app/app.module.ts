c49101a7c681190a668ecf836419adab7da3780e
Behzad NEKOUEI <behzadnekouei@gmail.com>
2022-06-29 10:55:40 +0200

    accounts component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CustomersComponent } from './customers/customers.component';
+import { AccountsComponent } from './accounts/accounts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CustomersComponent
+    CustomersComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule,
