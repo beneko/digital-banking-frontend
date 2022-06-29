06e52769e52e1eb88f7289b462d4dd2eb1cf8afb
Behzad NEKOUEI <behzadnekouei@gmail.com>
2022-06-29 14:17:36 +0200

    HttpClientModule added and used to get customers from backend

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  customers : any;
 
   ngOnInit(): void {
+    this.httpClient.get("http://localhost:8080/v1/customers").subscribe(data => {
+      this.customers = data;
+    })
   }
 
 }
